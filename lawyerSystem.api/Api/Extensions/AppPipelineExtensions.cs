3ce56c367bcb6c2423ab689fb35da8b5a4191606
GustavoSacoman <gustavo.sacoman23@gmail.com>
2025-06-30 19:26:51 -0300

    Created all files that set up the program.cs

---
+++ lawyerSystem.api/Api/Extensions/AppPipelineExtensions.cs
@@ -0,0 +1,21 @@
+﻿namespace lawyerSystem.api.Api.Extensions;
+
+public static class AppPipelineExtensions
+{
+    public static void UseApiPipeline(
+        this WebApplication app,
+        IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseCors("AllowAll");
+        app.UseHttpsRedirection();
+        app.UseAuthentication();
+        app.UseAuthorization();
+        app.MapControllers();
+    }
+}
---
+++ lawyerSystem.api/Api/Extensions/DependencyInjection.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
+
+namespace lawyerSystem.api.Api.Extensions;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApiServices(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+
+        services.AddControllers();
+
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen(options =>
+        {
+            options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            {
+                Name = "Authorization",
+                Type = SecuritySchemeType.ApiKey,
+                Scheme = "Bearer",
+                BearerFormat = "JWT",
+                In = ParameterLocation.Header,
+                Description = "JWT Authorization header using bearer scheme",
+            });
+            options.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference { Type = ReferenceType.SecurityScheme, Id = "Bearer" },
+                    },
+                    new string[] { }
+
+                },
+            });
+        });
+
+        var key = Encoding.ASCII.GetBytes(configuration["JwtSettings:SecretKey"]); // Pega a chave do appsettings
+        services.AddAuthentication(x =>
+        {
+            x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        })
+        .AddJwtBearer(x =>
+        {
+            x.RequireHttpsMetadata = false; // Em produção, deve ser true
+            x.SaveToken = true;
+            x.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(key),
+                ValidateIssuer = false, // Em produção, pode validar o emissor
+                ValidateAudience = false // Em produção, pode validar a audiência
+            };
+        });
+
+        services.AddCors(options =>
+        {
+            options.AddPolicy("AllowAll", builder =>
+                builder.AllowAnyOrigin()
+                       .AllowAnyMethod()
+                       .AllowAnyHeader());
+        });
+
+        return services;
+    }
+}
