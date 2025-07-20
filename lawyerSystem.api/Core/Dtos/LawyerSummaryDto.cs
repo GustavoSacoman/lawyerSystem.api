2f94e54ff171bac74d1e483dcb0f921ed33238bf
GustavoSacoman <gustavo.sacoman23@gmail.com>
2025-07-20 17:33:34 -0300

    Created Dtos To Lawyer

---
+++ lawyerSystem.api/Core/Dtos/LawyerSummaryDto.cs
@@ -0,0 +1,20 @@
+﻿namespace lawyerSystem.api.Core.Dtos;
+
+public class LawyerSummaryDto
+{
+    public Guid UserId { get; set; }
+
+    public string Name { get; set; }
+
+    public string OABNumber { get; set; }
+
+    public string Position { get; set; }
+
+    public LawyerSummaryDto(Guid userId, string name, string oabNumber, string position)
+    {
+        UserId = userId;
+        Name = name;
+        OABNumber = oabNumber;
+        Position = position;
+    }
+}
