16c322fb4fd4d5dc11b087851ea86cc0f1548c7b
GustavoSacoman <gustavo.sacoman23@gmail.com>
2025-07-24 18:51:10 -0300

    Feat: Created Interface for RoleRepository

---
+++ lawyerSystem.api/Core/Interfaces/IRoleRepository.cs
@@ -0,0 +1,20 @@
+﻿using lawyerSystem.api.Domain.Models;
+
+namespace lawyerSystem.api.Core.Interfaces;
+
+public interface IRoleRepository
+{
+    /// <summary>
+    /// Adds a new role to the repository.
+    /// </summary>
+    /// <param name="role">The role to add.</param>
+    /// <returns>A task representing the asynchronous operation.</returns>
+    Task AddAsync(Role role);
+
+    public Task<Role> GetRoleAsync(Guid Id);
+
+    public Task<Role> GetRoleByName(string name);
+    public Task UpdateRoleAsync(Role role);
+
+    public Task DeleteRoleAsync(Guid Id);
+}
