a6151693b50f6999f8d8444fbcfa05c14badfeae
GustavoSacoman <gustavo.sacoman23@gmail.com>
2025-07-24 19:00:56 -0300

    Feat: Added a Role and UserRole in AppDbContext and overrided the funciton OnModelCreating

--- lawyerSystem.api/Infrastructure/Data/AppDbContext.cs
+++ lawyerSystem.api/Infrastructure/Data/AppDbContext.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using lawyerSystem.api.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
 
 namespace lawyerSystem.api.Infrastructure.Data;
 
@@ -7,9 +9,16 @@ public class AppDbContext : DbContext
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
     {
     }
-    // Define DbSets for your entities here, e.g.:
-    // public DbSet<User> Users { get; set; }
-    // public DbSet<Role> Roles { get; set; }
-    // public DbSet<UserRole> UserRoles { get; set; }
+
+    public DbSet<Role> Roles { get; set; }
+
+    public DbSet<UserRole> UserRoles { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
 
 }
