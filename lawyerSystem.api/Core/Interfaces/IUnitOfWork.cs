83fb1125b1caa32de0ab866b7e5bb52d7c8d665e
GustavoSacoman <gustavo.sacoman23@gmail.com>
2025-07-24 18:38:19 -0300

    Created an interface of UnitOfWork

---
+++ lawyerSystem.api/Core/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿namespace lawyerSystem.api.Core.Interfaces;
+
+public interface IUnitOfWork : IDisposable
+{
+    IUserProfileRepository Users { get; }
+
+    ILawyerProfileRepositoyry Lawyers { get; }
+
+    IRoleRepository Roles { get; }
+
+    Task<int> SaveChangesAsync();
+}
