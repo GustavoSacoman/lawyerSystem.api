1f4c7b2323831bc64513e72ed61e43e9edd6ccad
GustavoSacoman <gustavo.sacoman23@gmail.com>
2025-07-25 23:20:03 -0300

    Feat: Created RoleMapper

---
+++ lawyerSystem.api/Core/Mappers/RoleMapper.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using lawyerSystem.api.Core.Dtos.Role;
+using lawyerSystem.api.Domain.Models;
+
+namespace lawyerSystem.api.Core.Mappers;
+
+public class RoleMapper : Profile
+{
+    public RoleMapper()
+    {
+        CreateMap<Role, RoleDto>();
+
+        CreateMap<CreateRoleCommand, Role>();
+    }
+}
