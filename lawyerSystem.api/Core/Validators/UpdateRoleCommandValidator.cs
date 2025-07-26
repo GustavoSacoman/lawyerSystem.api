b0578e5cea0774baa0baa0e59d257cdd5a19d984
GustavoSacoman <gustavo.sacoman23@gmail.com>
2025-07-25 23:36:16 -0300

    Feat: Created input validators

---
+++ lawyerSystem.api/Core/Validators/UpdateRoleCommandValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using lawyerSystem.api.Core.Dtos.Role;
+
+namespace lawyerSystem.api.Core.Validators;
+
+public class UpdateRoleCommandValidator : AbstractValidator<UpdateRoleCommand>
+{
+    public UpdateRoleCommandValidator()
+    {
+        RuleFor(x => x.Name)
+            .NotEmpty().WithMessage("Role name is required.")
+            .MinimumLength(3).WithMessage("Role name must be at least 3 characters long.")
+            .MaximumLength(50).WithMessage("Role name must not exceed 50 characters.");
+    }
+}
