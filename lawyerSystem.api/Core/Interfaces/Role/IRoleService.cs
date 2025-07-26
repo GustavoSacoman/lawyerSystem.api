208d1e097eabf218dca7b2cf5b82ca1290b3e1f2
GustavoSacoman <gustavo.sacoman23@gmail.com>
2025-07-25 23:19:23 -0300

    Feat: Created RoleService

---
+++ lawyerSystem.api/Core/Interfaces/Role/IRoleService.cs
@@ -0,0 +1,17 @@
+﻿using lawyerSystem.api.Core.Dtos.Role;
+namespace lawyerSystem.api.Core.Interfaces;
+
+public interface IRoleService
+{
+    public Task<RoleDto> CreateRoleAsync(CreateRoleCommand roleCommand);
+
+    public Task<IEnumerable<RoleDto>> GetAllRolesAsync();
+
+    public Task<RoleDto> GetRoleByName(string Name);
+
+    public Task<RoleDto> GetRoleByIdAsync(Guid Id);
+
+    public Task UpdateRole(Guid Id,UpdateRoleCommand roleCommand);
+
+
+}
